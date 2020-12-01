b286c915da4917c9f8c7c2085eec37b62e0a2ab6
Steve Feehan <stevef00@pobox.com>
2020-12-01 17:56:00 -0500

    cards: pass some fake data to home.html template

--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+cards = [
+    {
+        'owner': 'stevef',
+        'front': 'the train station',
+        'back':  'der Bahnhof',
+        'date_created': 'December 1, 2020',
+    },
+    {
+        'owner': 'stevef',
+        'front': 'the car',
+        'back':  'das Auto',
+        'date_created': 'December 1, 2020',
+    },
+]
+
 def home(request):
-    return render(request, 'cards/home.html')
+    context = {
+        'cards': cards,
+    }
+    return render(request, 'cards/home.html', context)
 
 def about(request):
     return render(request, 'cards/about.html')
