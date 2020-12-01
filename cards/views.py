bded6b0b26b321ec9fef736f4875c0feaf106ef1
Steve Feehan <stevef00@pobox.com>
2020-12-01 17:36:24 -0500

    cards: use template for cards-home route

--- cards/views.py
+++ cards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>DE Cards Home</h1>')
+    return render(request, 'cards/home.html')
 
 def about(request):
     return HttpResponse('<h1>Put Cards About Page Here</h1>')
