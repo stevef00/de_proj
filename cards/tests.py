ba0376ea0eb58a7edfc289f49da90e68c895a308
Steve Feehan <stevef00@pobox.com>
2020-12-01 15:14:18 -0500

    make cards-home the root for the project
    And update tests to reflect new routes.

--- cards/tests.py
+++ cards/tests.py
@@ -5,10 +5,10 @@ class CardsTestClass(TestCase):
 
     def test_root_view(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_cards_home_view(self):
-        response = self.client.get('/cards/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
         self.assertRegex(response.content, re.compile(b"<h1>DE Cards Home</h1>", re.IGNORECASE))
@@ -17,7 +17,7 @@ class CardsTestClass(TestCase):
         #
         # Note: URL needs a trailing '/' or we get a 301
         #
-        response = self.client.get('/cards/about/')
+        response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
 
         self.assertRegex(response.content, re.compile(b"<h1>Put Cards About Page Here</h1>", re.IGNORECASE))
