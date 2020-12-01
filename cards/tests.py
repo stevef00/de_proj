7136f654423fee7df0d4121194afe7f51c5e32bd
Steve Feehan <stevef00@pobox.com>
2020-12-01 14:09:14 -0500

    cards: add first test case
    I think these tests are incorrect in that they're in the 'cards'
    app but they're testing via the project urls.py routes. But some
    test is better than no test until I learn how to do it right.

--- cards/tests.py
+++ cards/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+import re
 
-# Create your tests here.
+class CardsTestClass(TestCase):
+
+    def test_root_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 404)
+
+    def test_cards_home_view(self):
+        response = self.client.get('/cards/')
+        self.assertEqual(response.status_code, 200)
+
+        self.assertRegex(response.content, re.compile(b"<h1>DE Cards Home</h1>", re.IGNORECASE))
