53926cf3cfad64342ecc28172c3f3729336030a2
Steve Feehan <stevef00@pobox.com>
2020-12-02 18:17:13 -0500

    users: boilerplate for 'users' app

--- de_proj/settings.py
+++ de_proj/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'cards.apps.CardsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
