1434360ae7ab5842b1f524adbfadbd1096d1016b
Steve Feehan <stevef00@pobox.com>
2020-12-02 20:50:41 -0500

    add login/logout views with custom templates

--- users/views.py
+++ users/views.py
@@ -7,9 +7,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('cards-home')
+            messages.success(request, f'Your account has been created. You are now able to login')
+            return redirect('login')
     else:
         form = UserRegisterForm()
 
