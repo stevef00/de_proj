43fd83ed27c020872826e3db2ec176d3f7485c8c
Steve Feehan <stevef00@pobox.com>
2020-12-02 19:14:04 -0500

    users: start using crispy-forms
    Add to project settings and boilerplate in register.html. Not using
    any CSS framework yet, so not really getting much out of it.

--- de_proj/settings.py
+++ de_proj/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cards.apps.CardsConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
