3cb2747c9ee211842afd75285e06766e78b603d0
Steve Feehan <stevef00@pobox.com>
2020-12-04 17:26:13 -0500

    cards: add CardCreateView with form

--- cards/urls.py
+++ cards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CardListView, CardDetailView
+from .views import CardListView, CardDetailView, CardCreateView
 from . import views
 
 urlpatterns = [
     path('', CardListView.as_view(), name='cards-home'),
     path('card/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
+    path('card/new/', CardCreateView.as_view(), name='card-create'),
     path('about/', views.about, name='cards-about')
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse
 from .models import Card
 
 class CardListView(ListView):
@@ -11,6 +12,24 @@ class CardListView(ListView):
 
 class CardDetailView(DetailView):
     model = Card
+
+class CardCreateView(CreateView):
+    model = Card
+    fields = ['front', 'back']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    #
+    # Determine the URL to redirect to when the form is successfully validated.
+    # Returns success_url by default.
+    #
+    # FIXME: when we have Decks we'll want to redirect to the Deck
+    # the card is added to.
+    #
+    def get_success_url(self):
+        return reverse('cards-home')
 
 def about(request):
     return render(request, 'cards/about.html', {'title': 'About'})
