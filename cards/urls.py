7583561674b5101351c40d3b25eaa82994426c2d
Steve Feehan <stevef00@pobox.com>
2020-12-04 18:12:02 -0500

    cards: fix card/<int:pk>/update/ url

--- cards/urls.py
+++ cards/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', CardListView.as_view(), name='cards-home'),
     path('card/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
     path('card/new/', CardCreateView.as_view(), name='card-create'),
-    path('card/update/<int:pk>/', CardUpdateView.as_view(), name='card-update'),
+    path('card/<int:pk>/update/', CardUpdateView.as_view(), name='card-update'),
     path('card/<int:pk>/delete/', CardDeleteView.as_view(), name='card-delete'),
     path('about/', views.about, name='cards-about')
 ]
