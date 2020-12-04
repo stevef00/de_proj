56c95bc05b536dbd2a9b56ba5b7bab582c388ef3
Steve Feehan <stevef00@pobox.com>
2020-12-04 16:34:27 -0500

    cards: add CardDetailView and link to cards in home.html

--- cards/urls.py
+++ cards/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CardListView
+from .views import CardListView, CardDetailView
 from . import views
 
 urlpatterns = [
     path('', CardListView.as_view(), name='cards-home'),
+    path('card/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
     path('about/', views.about, name='cards-about')
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Card
 
 class CardListView(ListView):
@@ -8,6 +8,9 @@ class CardListView(ListView):
     template_name = 'cards/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'cards'
     ordering = ['-date_created']
+
+class CardDetailView(DetailView):
+    model = Card
 
 def about(request):
     return render(request, 'cards/about.html', {'title': 'About'})
