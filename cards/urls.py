bfb34d33f08d08259f9829ee2f0f8aeef49c1958
Steve Feehan <stevef00@pobox.com>
2020-12-04 17:43:51 -0500

    cards: add CardUpdateView
    Only allow the card author to update the card.

--- cards/urls.py
+++ cards/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from .views import CardListView, CardDetailView, CardCreateView
+from .views import (
+        CardListView,
+        CardDetailView,
+        CardCreateView,
+        CardUpdateView
+)
 from . import views
 
 urlpatterns = [
     path('', CardListView.as_view(), name='cards-home'),
     path('card/<int:pk>/', CardDetailView.as_view(), name='card-detail'),
     path('card/new/', CardCreateView.as_view(), name='card-create'),
+    path('card/update/<int:pk>/', CardUpdateView.as_view(), name='card-update'),
     path('about/', views.about, name='cards-about')
 ]
--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import (
+        ListView,
+        DetailView,
+        CreateView,
+        UpdateView
+)
 from django.urls import reverse
 from .models import Card
 
@@ -21,6 +26,31 @@ class CardCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+    #
+    # Determine the URL to redirect to when the form is successfully validated.
+    # Returns success_url by default.
+    #
+    # FIXME: when we have Decks we'll want to redirect to the Deck
+    # the card is added to.
+    #
+    def get_success_url(self):
+        return reverse('cards-home')
+
+class CardUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Card
+    fields = ['front', 'back']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
     #
     # Determine the URL to redirect to when the form is successfully validated.
